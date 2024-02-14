d5d20869b1216e49d321fbd34fa4bc7cb9f780d6
Chamele <114728861+Chamele-listens@users.noreply.github.com>
2024-02-14 15:08:15 +0700

    Add function to create squares in a grid

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 const body = document.querySelector("body")
-const container = document.createElement("div")
+const containerY = document.createElement("div")
 
-let boxNum = prompt("How many boxes would you like?")
+
+containerY.style.display = "flex"
+containerY.style.flexWrap = "wrap"
+containerY.style.flexDirection = "column"
+
+
+
+let boxNum = prompt("what grid would you like?");
 
 for (let i = 1; i <= boxNum; i++){
-    const box = document.createElement("div")
-
-    box.style.backgroundColor = "blue"
-    box.style.border = "solid 1px black"
-    box.style.height = "50px"
-    box.style.width = "50px"
-
-    container.appendChild(box)
-    console.log(`${i} box`)
+    const containerX = document.createElement("div")
+    containerX.style.display = "flex"
+    containerX.style.flexWrap = "wrap"
+    for (let i = 1; i <= boxNum; i++){
+        const box = document.createElement("div")
+    
+        box.style.backgroundColor = "blue"
+        box.style.border = "solid 1px black"
+        box.style.height = "50px"
+        box.style.width = "50px"
+    
+        containerX.appendChild(box)
+    }
+    containerY.append(containerX);
 }
 
 
 
-body.appendChild(container);
+
+body.appendChild(containerY);
