e3eb99abddc2a05df3cce9e51c6ed301b5c976c6
Chamele <114728861+Chamele-listens@users.noreply.github.com>
2024-02-14 16:29:33 +0700

    Add restart function
    Known bug: Removing containerY doesn't actually remove the previous
    container. It instead add them together with the new container and
    doesn't restart the grid.

--- script.js
+++ script.js
@@ -7,41 +7,46 @@ containerY.style.display = "flex"
 containerY.style.flexWrap = "wrap"
 containerY.style.flexDirection = "column"
 
-boxGenerate.addEventListener("click",()=>{
+boxGenerate.addEventListener("click",()=>{  
     boxNum = prompt("what grid would you like?");
-    console.log(boxNum)
+    if (containerY != null){
+        containerY.remove();
+    }
+    for (let i = 1; i <= boxNum; i++){
+        const containerX = document.createElement("div")
+        containerX.style.display = "flex"
+        containerX.style.flexWrap = "wrap"
+        for (let i = 1; i <= boxNum; i++){
+            const box = document.createElement("div")
+            box.classList.add("box-color");
+        
+            box.style.backgroundColor = "blue"
+            box.style.border = "solid 1px black"
+            box.style.height = "50px"
+            box.style.width = "50px"
+        
+            containerX.appendChild(box)
+        }
+        containerY.append(containerX);
+    }
+    body.appendChild(containerY);
+    let boxColor = document.querySelectorAll(".box-color")
+    console.log(boxColor);
+
+    boxColor.forEach((box)=>{
+        box.addEventListener("mouseover", ()=>{
+            box.style.backgroundColor = "red"
+        });
+});
 })
 
 
 
-for (let i = 1; i <= boxNum; i++){
-    const containerX = document.createElement("div")
-    containerX.style.display = "flex"
-    containerX.style.flexWrap = "wrap"
-    for (let i = 1; i <= boxNum; i++){
-        const box = document.createElement("div")
-        box.classList.add("box-color");
-    
-        box.style.backgroundColor = "blue"
-        box.style.border = "solid 1px black"
-        box.style.height = "50px"
-        box.style.width = "50px"
-    
-        containerX.appendChild(box)
-    }
-    containerY.append(containerX);
-}
 
-body.appendChild(containerY);
 
-let boxColor = document.querySelectorAll(".box-color")
-console.log(boxColor);
 
-boxColor.forEach((box)=>{
-    box.addEventListener("mouseover", ()=>{
-        box.style.backgroundColor = "red"
-    });
-});
+
+
 
 
 
